bc2c5e0369333ffe4ba79f73a16ad68985c603ae
David Hernando Sancha <david.hernando@2manylanguages.com>
2018-02-10 09:34:51 +0100

    A Dummy Commit of new lines

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        // new line
+        // new line
+        // new line
+        // new line
+        // new line
+        // new line
 	// write your code here so dummy
+        // new line
+        // new line
+        // new line
+        // new line
     }
 }
